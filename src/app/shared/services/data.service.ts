54f9db5d79cbc7aa96e7ac29af254a9894f0973c
zahariev <zaharievboyan@gmail.com>
2023-08-03 08:22:50 +0300

    fix: refactor dataService

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -2,70 +2,29 @@ import { Injectable, signal } from '@angular/core';
 import data from '../data.json';
 import { Contact } from '../models/contact.model';
 import { Group } from '../models/group.model';
-import { BehaviorSubject, of } from 'rxjs';
+import {
+  BehaviorSubject,
+  Observable,
+  debounceTime,
+  of,
+  shareReplay,
+  switchMap,
+  tap,
+} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
+// A Service for fetching data from a JSON file
 export class DataService {
-  public contacts: BehaviorSubject<Contact[]> = new BehaviorSubject<Contact[]>(
-    []
-  );
-  public groups: BehaviorSubject<Group[]> = new BehaviorSubject<Group[]>([]);
+  private readonly JSON_DATA_URL = '../../../assets/data.json';
 
-  private _invitedContacts: Set<Contact> = new Set();
-  public invitedContacts = signal(this._invitedContacts);
+  constructor(private readonly httpClient: HttpClient) {}
 
-  constructor() {
-    this.contacts.next(data as any);
-    this.groups.next(this.getUniqueGroups(data));
-  }
-
-  inviteGroup(group: Group) {
-    const contacts: Contact[] = [];
-    group.contacts.forEach((contact) =>
-      this.invitedContacts.mutate((values) => values.add(contact))
-    );
-
-    this.invitedContacts.mutate((values) => [...values, ...contacts]);
-  }
-
-  invite(contact: Contact) {
-    this.invitedContacts.mutate((values) => values.add(contact));
-  }
-
-  removeInvitation(contact: Contact) {
-    this.invitedContacts.mutate((values) => values.delete(contact));
-  }
-
-  clearAllInvitations() {
-    this.invitedContacts.mutate((values) => values.clear());
-  }
-
-  private getUniqueGroups(data: any[]): Group[] {
-    const groupsWithChildRecords: Group[] = [];
-
-    data.forEach((contact) => {
-      // Check if the contact has groups
-      if (contact.groups && Array.isArray(contact.groups)) {
-        // Loop through each group in the contact
-        contact.groups.forEach((group: string) => {
-          // Check if the group already exists
-          const existingGroup = groupsWithChildRecords.findIndex(
-            (item) => item.name === group
-          );
-
-          // Group doesn't exist yet, so add it
-          if (existingGroup === -1) {
-            groupsWithChildRecords.push({ name: group, contacts: [contact] });
-          } else {
-            // Group already exists, so add the contact to it
-            groupsWithChildRecords[existingGroup].contacts.push(contact);
-          }
-        });
-      }
-    });
-
-    return groupsWithChildRecords;
+  getData(): Observable<Contact[]> {
+    return this.httpClient
+      .get<Contact[]>(this.JSON_DATA_URL)
+      .pipe(shareReplay(1));
   }
 }
